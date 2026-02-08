efc386136587fb161b064c5a555e28aa47dd178e
suresh228devops <suresh228devops@gmail.com>
2026-02-08 23:21:05 +0530

    terraform-multi-env

---
+++ workspaces/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.30.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "ksrdaws84s-remote-state"
+    key    = "workspace-demo"
+    region = "us-east-1"
+    #dynamodb_table = "ksrdaws84s-remote-state"
+    encrypt = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
